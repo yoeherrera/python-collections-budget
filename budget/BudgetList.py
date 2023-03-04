ef445bcf0bb26304e11c13eef48b5f699acb682a
yoe <yoe@mmeta4.capital>
2023-03-03 22:23:40 -0500

    Completed module￼

---
+++ budget/BudgetList.py
@@ -0,0 +1,33 @@
+from . import Expense
+
+class BudgetList:
+    def __init__(self, budget):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
+
+    def append(self, item):
+        if self.sum_expenses + item < self.budget:
+            self.expenses.append(item)
+            self.sum_expenses += item
+        else:
+            self.overages.append(item)
+        
+    def __len__(self):
+        return len(self.sum_expenses) + len(self.overages)
+    
+def main():
+    myBudgetList = BudgetList(budget=1200)
+    expenses = Expense.Expenses()
+    expenses.read_expenses('data/spending_data.csv')
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+
+    print('The count of all expenses: ' + str(myBudgetList))
+
+if __name__ == '__main__':
+    main()
+
+
